dfc6df7ddb6c18736d12c2ef40ce2ece2a43693a
Helgezec <helgezec@gmail.com>
2022-09-26 08:42:36 +0300

    razor library usage by maui application

--- Maui/MauiProgram.cs
+++ Maui/MauiProgram.cs
@@ -1,10 +1,10 @@
-﻿using Maui.Data;
-using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
 
 namespace Maui
 {
     public static class MauiProgram
     {
+        private const string webApiHostAddress = "https://localhost:7169/";
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -19,8 +19,7 @@ namespace Maui
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-
-            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(webApiHostAddress) });
 
             return builder.Build();
         }
